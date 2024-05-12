e36444d5c959b1182c47f0fd94c19ebf63a93f46
Matthew Bunce <77083766+MPBunce@users.noreply.github.com>
2024-05-12 14:45:31 -0400

    shunting yard

--- cli_calculator/src/main.rs
+++ cli_calculator/src/main.rs
@@ -1,12 +1,21 @@
 use std::env;
+extern crate rustyard;
 
 fn main() {
     
     let args: Vec<String> = env::args().collect();
 
-    for n in &args{
+    for n in args.clone(){
         println!("{}", n)
     }
+    println!("Calulcating result of: {}", &args[1]);
+    let res = parse_string(args[1].to_string());
 
-    println!("Hello, world!");
+    println!("Result: {}", res);
+}
+
+fn parse_string(input: String) -> f64{
+    let mut yard = rustyard::ShuntingYard::new();
+    let a:f64 = yard.calculate(&input).unwrap();
+    return a
 }
